335a569bd5a06f0dae4bedfb7a2201d7140be584
Astrid <77618324+AstridWest@users.noreply.github.com>
2021-06-04 12:10:19 +0200

    glemte en lille ting

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ require("dotenv").config();
 const { request } = require("express");
 const express = require("express"); //express er serveren
 const formidable = require("express-formidable");
+const cors = require("cors")
 const animals = require("./routes/animals");
 const foods = require("./routes/foods");
-const accessories = require("./routes/accessories")
+const accessories = require("./routes/accessories");
 
 //set up express app
 const app = express();
 
 //import db-database
-require("./database")
+require("./database");
+
+app.use("/", express.static('docs'))
+
+//allows request from other origins
+app.use(cors())
 
 //parse http form data
 app.use(formidable());
